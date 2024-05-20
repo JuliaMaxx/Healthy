b384b89df46d8fbd2a69f4bbcf05b027b819d6f9
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-05-20 14:15:38 +0200

    refactor: generate html for home oop way

--- Healthy/Controllers/HomeController.cs
+++ Healthy/Controllers/HomeController.cs
@@ -1,16 +1,101 @@
 using Healthy.Models;
+using Html;
+using ILogger;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
+namespace Html
+{
+    public abstract class BaseHtmlElement
+    {
+        public abstract string Render();
+    }
+    public class HtmlHeader : BaseHtmlElement
+    {
+        private string _content;
+
+        public HtmlHeader(string content)
+        {
+            _content = content;
+        }
+
+        public override string Render()
+        {
+            return $"<h1>{_content}</h1>";
+        }
+    }
+    public class HtmlParagraph : BaseHtmlElement
+    {
+        private string _content;
+
+        public HtmlParagraph(string content)
+        {
+            _content = content;
+        }
+
+        public override string Render()
+        {
+            return $"<p>{_content}</p>";
+        }
+    }
+}
+
+namespace ILogger
+{
+    public interface ILoggerService
+    {
+        void Log(string message);
+    }
+
+    public class ConsoleLoggerService : ILoggerService
+    {
+        public void Log(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
+
+namespace Singleton
+{
+    public class SingletonService
+    {
+        private static SingletonService _instance;
+        private static readonly object _lock = new object();
+
+        private SingletonService()
+        {
+        }
+
+        public static SingletonService Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonService();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+
+}
 
 namespace Healthy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILoggerService _loggerService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ILoggerService loggerService)
         {
             _logger = logger;
+            _loggerService = loggerService;
         }
 
         public IActionResult Index()
@@ -21,6 +106,15 @@ namespace Healthy.Controllers
             }
             else
             {
+                var elements = new List<BaseHtmlElement>
+                {
+                    new HtmlHeader("Welcome"),
+                    new HtmlParagraph("Learn about <a href='https://learn.microsoft.com/aspnet/core'>building Web apps with ASP.NET Core</a>.")
+                };
+
+                _loggerService.Log("Rendering Index page.");
+
+                ViewBag.Elements = elements;
                 return View();
             }
         }
--- Healthy/Program.cs
+++ Healthy/Program.cs
@@ -1,4 +1,5 @@
 using Healthy.Data;
+using ILogger;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<ILoggerService, ConsoleLoggerService>();
 
 var app = builder.Build();
 
